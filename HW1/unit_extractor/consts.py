0337f2b80a64496bee975438590cf02a2d98d1fb
ArshiAAkhavan <letmemakenewone@gmail.com>
2022-04-18 00:44:45 +0430

    refactor regexes

--- HW1/unit_extractor/consts.py
+++ HW1/unit_extractor/consts.py
@@ -5,18 +5,18 @@ UNIT_TRASH_MAGIC = "⌡"
 RESERVED_TRASH_MAGIC = "Ж"
 ANOTHER_RESERVED_TRASH_MAGIC = "Д"
 
-num_patterns = f"{NUMBER_TRASH_MAGIC}+"
-unit_patterns = f"{UNIT_TRASH_MAGIC}+"
-word_patterns = "[\u0600-\u06ff]+"
 
 NUMBER_PATTERN_NAME = "number"
 UNIT_PATTERN_NAME = "unit"
 ITEM_PATTERN_NAME = "item"
+
+num_pattern = f"(?P<{NUMBER_PATTERN_NAME}>{NUMBER_TRASH_MAGIC}+)"
+unit_pattern = f"(?P<{UNIT_PATTERN_NAME}>{UNIT_TRASH_MAGIC}+)"
+item_pattern = f"(?P<{ITEM_PATTERN_NAME}>[\u0600-\u06ff]+)"
 patterns = [
-    f"(?P<{NUMBER_PATTERN_NAME}>{num_patterns})\s+(?P<{UNIT_PATTERN_NAME}>{unit_patterns})+\s+(?P<{ITEM_PATTERN_NAME}>{word_patterns})",
-    f"(?P<{NUMBER_PATTERN_NAME}>{num_patterns})\s+(?P<{UNIT_PATTERN_NAME}>{unit_patterns})+\s+(?P<{ITEM_PATTERN_NAME}>{word_patterns})",
+    rf"{num_pattern}\s+{unit_pattern}\s+{item_pattern}",
 ]
 
-unit_overlap_regex = re.compile(f'({UNIT_TRASH_MAGIC}+)(\s+)(?:{UNIT_TRASH_MAGIC})')
+unit_overlap_regex = re.compile(
+    rf'({UNIT_TRASH_MAGIC}+)(\s+)(?:{UNIT_TRASH_MAGIC})')
 pattern_regex = [re.compile(pattern) for pattern in patterns]
-
